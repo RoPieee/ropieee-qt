977d0da084d29b0ade8d4ba72dd46d98d725ef7e
spockfish <htenberge@gmail.com>
2018-03-31 09:48:53 +0200

    bump package

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,8 +1,4 @@
 # this script setups a dev environment
 
-# seems this is still not fixed
-ln -s /etc/ca-certificates/extracted/ca-bundle.trust.crt /etc/ssl/certs/ca-certificates.crt
-
-
-pacman -Syu libfbclient libmariadbclient unixodbc postgresql-libs gst-plugins-base-libs gtk3 libpulse cups freetds fakeroot patch
+pacman -Syu libfbclient libmariadbclient unixodbc postgresql-libs gst-plugins-base-libs gtk3 libpulse cups freetds fakeroot patch binutils make gcc
 
