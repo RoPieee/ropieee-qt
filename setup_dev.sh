4584548809e6b79df0e91161c3e12addbcb68b47
spockfish <htenberge@gmail.com>
2018-05-14 22:37:27 +0200

    bump packages

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,4 +1,4 @@
 # this script setups a dev environment
 
-pacman -Syu libfbclient libmariadbclient unixodbc postgresql-libs gst-plugins-base-libs gtk3 libpulse cups freetds fakeroot patch binutils make gcc
+pacman -Syu --needed libfbclient libmariadbclient unixodbc postgresql-libs gst-plugins-base-libs gtk3 libpulse cups freetds fakeroot patch binutils make gcc
 
