3d7ede57ccec188ccb3e04baa688431f8a4a0886
Spockfish <htenberge+ropieee@gmail.com>
2020-03-16 21:16:58 +0100

    make it work on the pi4

--- setup_dev.sh
+++
@@ -1,4 +0,0 @@
-# this script setups a dev environment
-
-pacman -Syu --needed libfbclient libmariadbclient unixodbc postgresql-libs gst-plugins-base-libs gtk3 libpulse cups freetds fakeroot patch binutils make gcc
-
